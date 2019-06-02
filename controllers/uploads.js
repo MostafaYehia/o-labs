7938dab04d990b97f52901ba0a7ed5274dd8ef34
Mostafa Yehia <mostafayehia212@gmail.com>
2019-06-02 15:28:10 +0200

    validate contact avatar size and type

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -15,12 +15,12 @@ exports.configFileStorage = (fieldName, folderPath, fileTypes) => {
   return multer({
     storage: storage,
     limits: {
-      fileSize: 1024 * 1024 * 5
+      fileSize: 1024 * 1024 * 1
     },
     fileFilter: (req, file, cb) => {
       const ext = file.mimetype.split("/")[1];
-      if (!fileTypes.includes(ext)) cb("Invalid file type", false);
-      else cb(null, true);
+      if (!fileTypes.includes(ext)) return cb("Invalid file type", false);
+      cb(null, true);
     }
   }).single(fieldName);
 };
@@ -28,9 +28,12 @@ exports.configFileStorage = (fieldName, folderPath, fileTypes) => {
 exports.errorHandler = uploader => {
   return (req, res, next) => {
     uploader(req, res, err => {
-      console.log(err);
+      let message = err.message || err;
+
+      if (message == "File too large") message = "Maximum size is 2Mb";
+
       if (err) {
-        return res.status(415).json({ message: err });
+        return res.status(415).json({ message });
       }
       next();
     });
