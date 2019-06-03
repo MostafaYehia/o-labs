36af16fc95441b2847a9da8e95ec11cb0892531f
Mostafa Yehia <mostafayehia212@gmail.com>
2019-06-03 13:48:17 +0200

    debug image resize method

--- controllers/images.js
+++ controllers/images.js
@@ -1,14 +1,7 @@
-const btoa = require('btoa');
+const btoa = require("btoa");
 const sharp = require("sharp");
 
 exports.resize = (id, path, size, type) => {
-  let originalPath = path.split(".");
-  let ext = originalPath[1];
-  let newSizePath = `${originalPath[0]}-${type}.${ext}`;
-
-  console.log("Orininal path: ", originalPath);
-  console.log("newSizePath path: ", newSizePath);
-
   return new Promise(async (resolve, reject) => {
     try {
       const buffer = await sharp(path)
@@ -16,7 +9,6 @@ exports.resize = (id, path, size, type) => {
         .toBuffer();
 
       let base64 = btoa(String.fromCharCode.apply(null, buffer));
-      console.log("encoded");
       base64 = `data:image/jpg;base64,${base64}`;
       resolve(base64);
     } catch (error) {
